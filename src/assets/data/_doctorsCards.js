93ba2589e1dfa380f9159b99fcaef180ecd66abb
Fernweh99 <daniele.dimento@gmail.com>
2022-08-23 16:37:04 +0200

    fix image data

--- src/assets/data/_doctorsCards.js
+++ src/assets/data/_doctorsCards.js
@@ -1,18 +1,18 @@
 export default [
   {
-    "source": "../img/doctor-1.jpg",
+    "source": require("../img/doctor-1.jpg"),
     "name": "Jon Snow",
     "role": "Anesthesiologist",
     "description": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium vulaptatum deleniti atque"
   },
   {
-    "source": "../img/doctor-2.jpg",
+    "source": require("../img/doctor-2.jpg"),
     "name": "Tony Stark",
     "role": "Cardiologist",
     "description": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium vulaptatum deleniti atque"
   },
   {
-    "source": "../img/doctor-3.jpg",
+    "source": require("../img/doctor-3.jpg"),
     "name": "Anna Smith",
     "role": "Nurse Practitioner",
     "description": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium vulaptatum deleniti atque"
--- src/assets/data/_facilitiesCards.js
+++ src/assets/data/_facilitiesCards.js
@@ -2,19 +2,19 @@ export default [
   {
     "title": "Emergency Care",
     "bgcolor": "aqua",
-    "source": "../img/wave-divider.png",
+    "source": require("../img/wave-divider.png"),
     "text": "Set ut perspiciatis unde omnis iste natus error sit voluptam accusantim",
   },
   {
     "title": "Expert Diagnostic",
     "bgcolor": "aqua",
-    "source": "../img/wave-divider.png",
+    "source": require("../img/wave-divider.png"),
     "text": "Set ut perspiciatis unde omnis iste natus error sit voluptam accusantim",
   },
   {
     "title": "Superb Rehabilitation",
     "bgcolor": "aqua",
-    "source": "../img/wave-divider.png",
+    "source": require("../img/wave-divider.png"),
     "text": "Set ut perspiciatis unde omnis iste natus error sit voluptam accusantim",
   },
 ]
