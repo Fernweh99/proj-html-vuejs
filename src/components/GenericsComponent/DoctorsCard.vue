47c92465d076f8e0e3b9a6d92991067a7eb71e53
Fernweh99 <daniele.dimento@gmail.com>
2022-08-25 09:56:31 +0200

    optimizd grafic doctors card

--- src/components/GenericsComponent/DoctorsCard.vue
+++ src/components/GenericsComponent/DoctorsCard.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
+  <div class="cs_card">
     <img :src="image" alt="">
-    <h3>{{ name }}</h3>
-    <h4>{{ role }}</h4>
-    <p>{{ description }}</p>
+    <div class="p-5">
+      <h3>{{ name }}</h3>
+      <h4>{{ role }}</h4>
+      <p class="py-3">{{ description }}</p>
+      <a class="facebook" href="https://www.facebook.com"><i class="fa-brands fa-square-facebook"></i></a>
+      <a class="twitter" href="https://www.twitter.com"><i class="fa-brands fa-square-twitter"></i></a>
+      <a class="instagram" href="https://www.instagram.com"><i class="fa-brands fa-square-instagram"></i></a>
+    </div>
   </div>
 </template>
 
@@ -20,7 +25,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  img {
-    max-width: 100%;
+  .cs_card {
+    background-color: white;
+    img {
+      max-width: 100%;
+    } h3,h4 {
+      font-size: 17px;
+      font-weight: 300;
+    } h3 {
+      text-transform: uppercase;
+    } p {
+      font-weight: 200;
+    } a {
+      &.facebook {
+        color: blue;
+      } &.twitter {
+        color: #54ACED;
+      } &.instagram {
+        color: #3E729A;
+      }
+      i {
+        font-size: 30px;
+      margin-right: 10px;
+      }
+    }
   }
 </style>
